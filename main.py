d4fbc65a32877f580ba962144626d3bd2df04484
haanhtuandev <tuanha41105@gmail.com>
2025-04-07 22:05:24 +0700

    implement movement for player

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
 import pygame
 from constants import *
-
+from player import *
 
 
 def main():
     pygame.init
+    clock = pygame.time.Clock()
+    dt = 0.0
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
 
     while True:
         pygame.Surface.fill(screen, (0,0,0))
-
-
+        player.update(dt)
+        player.draw(screen)
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
 
         pygame.display.flip()
-        print("Starting Asteroids!")
-        print(f"Screen width: {SCREEN_WIDTH}")
-        print(f"Screen height: {SCREEN_HEIGHT}")
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
