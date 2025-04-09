9f389b0b6bdbb68c4feda26849ff142ae78f6f18
haanhtuandev <tuanha41105@gmail.com>
2025-04-09 09:24:11 +0700

    implement Asteroids sprites

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
 import pygame
 from constants import *
 from player import *
-
-
+from asteroid import *
+from asteroidfield import *
 def main():
     pygame.init
     clock = pygame.time.Clock()
     dt = 0.0
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
+    asteroid_field = AsteroidField()
+
+# Change the game loop to use the new groups instead of the Player object directly.
+# Call the .update() method on the "updatables" group.
+# Loop over all "drawables" and .draw() them individually.
 
     while True:
         pygame.Surface.fill(screen, (0,0,0))
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        # Draw all objects in the `drawables` group
+        for sprite in drawable:
+            sprite.draw(screen)  # Calls .draw(screen) on each sprite
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
